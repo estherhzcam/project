6f285fa9f3b28d4247d0bbff924e9b770d3a5a4f
Esther <esth2577@stud.kea.dk>
2021-04-11 13:53:17 +0200

    last css + congrats message

--- host-article.js
+++ host-article.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form");
-form.addEventListener("submit", userSubmitted)
+form.addEventListener("submit", userSubmitted);
 
 function userSubmitted(e) {
   e.preventDefault();
@@ -9,37 +9,50 @@ function userSubmitted(e) {
   console.log(form.elements.content.value);
 
   const payload = {
-      email:form.elements.email.value,
-      username: form.elements.username.value,
-      title:form.elements.title.value,
-      content:form.elements.content.value,
-      date: Date.now()
-  }
+    email: form.elements.email.value,
+    username: form.elements.username.value,
+    title: form.elements.title.value,
+    content: form.elements.content.value,
+    date: Date.now(),
+  };
   document.querySelector("input[type=submit]").disabled = true;
 
-  fetch("https://keaprojects21-50cf.restdb.io/rest/guest-posts", 
-  {
+  fetch("https://keaprojects21-50cf.restdb.io/rest/guest-posts", {
     method: "POST",
     headers: {
       "x-apikey": "602e38155ad3610fb5bb62bd",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(payload)
+    body: JSON.stringify(payload),
   })
     .then((response) => {
       console.log(response);
 
       // add congrats message
-      
-document.querySelector("input[type=submit]").disabled =false;
-form.elements.title.value ="";
-form.elements.username.value ="";
-form.elements.email.value ="";
-form.elements.content.value ="";
-    })
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+      showCongratsMessage();
 
+      document.querySelector("input[type=submit]").disabled = false;
+      form.elements.title.value = "";
+      form.elements.username.value = "";
+      form.elements.email.value = "";
+      form.elements.content.value = "";
+    })
 
     .catch((err) => {
       console.error(err);
     });
-};
+}
+function showCongratsMessage() {
+  console.log("working")
+  document.querySelector("#congrats").classList.remove("hidden");
+  document.querySelector("#close").addEventListener("click", close);
+}
+
+function close(){
+  document.querySelector("#congrats").classList.add("hidden");
+} 
